0b5f4d139689d0686385e8f12e2408f71cb81ac1
NicolasArthurDev <nicolasarthur1806@gmail.com>
2026-05-03 17:24:58 -0300

    feat: implementa criação de notas via plugin com tratamento de erros no obsidian #50

--- .obsidian/plugins/obsidian-markupp-plugin/src/settings.ts
+++ .obsidian/plugins/obsidian-markupp-plugin/src/settings.ts
@@ -1,36 +1,37 @@
-import {App, PluginSettingTab, Setting} from "obsidian";
-import MyPlugin from "./main";
+import { App, PluginSettingTab, Setting } from "obsidian";
+import MarkuppPlugin from "./main";
 
-export interface MyPluginSettings {
-	mySetting: string;
+export interface MarkuppSettings {
+	backendUrl: string;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export const DEFAULT_SETTINGS: MarkuppSettings = {
+	backendUrl: "http://localhost:8080",
+};
 
-export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export class MarkuppSettingTab extends PluginSettingTab {
+	plugin: MarkuppPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: MarkuppPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
+		const { containerEl } = this;
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Backend URL")
+			.setDesc("Endereço do servidor Markupp para onde as notas serão enviadas.")
+			.addText((text) =>
+				text
+					.setPlaceholder("http://localhost:8080")
+					.setValue(this.plugin.settings.backendUrl)
+					.onChange(async (value) => {
+						this.plugin.settings.backendUrl = value.trim();
+						await this.plugin.saveSettings();
+					}),
+			);
 	}
 }
